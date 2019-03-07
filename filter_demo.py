46414b541cd4b1bd5c3db0a86ad542a1a165b269
Moritz <email@moritz-huebner.de>
2019-03-07 14:11:01 +1100

    PEP8

--- filter_demo.py
+++ filter_demo.py
@@ -4,6 +4,7 @@
 # Questions? Email: msmith@astro.swin.edu.au
 
 import matplotlib
+
 matplotlib.use('tkagg')
 import matplotlib.pyplot as plt
 from scipy import signal
@@ -13,24 +14,24 @@ import numpy as np
 plt.figure
 
 # Generate time
-t = np.linspace(0,0.2,2001)
+t = np.linspace(0, 0.2, 2001)
 # Compute genuine signal
-x = np.sin(2.0*np.pi*5.0*t) + 0.1*np.sin(2.0*np.pi*50.0*t)
-plt.plot(t,x,'k',alpha=0.75)
+x = np.sin(2.0 * np.pi * 5.0 * t) + 0.1 * np.sin(2.0 * np.pi * 50.0 * t)
+plt.plot(t, x, 'k', alpha=0.75)
 # Add some noise
-x = x+ np.random.randn(len(t))*0.5
+x = x + np.random.randn(len(t)) * 0.5
 
 # Peform filtering using Butterworth
-b,a = signal.butter(3,0.01)
-zi = signal.lfilter_zi(b,a)
-z, _ = signal.lfilter(b,a,x, zi=zi*x[0])
-filt_x = signal.filtfilt(b,a,x)
+b, a = signal.butter(3, 0.01)
+zi = signal.lfilter_zi(b, a)
+z, _ = signal.lfilter(b, a, x, zi=zi * x[0])
+filt_x = signal.filtfilt(b, a, x)
 
 # Plot
-plt.plot(t,x,'b',alpha=0.75)
-plt.plot(t,filt_x,'r')
+plt.plot(t, x, 'b', alpha=0.75)
+plt.plot(t, filt_x, 'r')
 plt.title('Comparison of filtered result and original signal')
 plt.xlabel('Time, s')
 plt.ylabel('Signal, x')
-plt.legend(('Real Signal','Signal+Noise','Filtered Result'))
+plt.legend(('Real Signal', 'Signal+Noise', 'Filtered Result'))
 plt.show()
